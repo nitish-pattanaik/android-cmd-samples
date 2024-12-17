6b359258a7d36e76e5d06f4c1c5917b122bf8e3e
Nitish Pattanaik <nitishkpattanaik@gmail.com>
2024-12-17 17:50:47 +0530

    initial commit before creation

---
+++ MyApp/src/com/example/myapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapp
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textView = TextView(this)
+        textView.text = "Hello, World!"
+        setContentView(textView)
+    }
+}
