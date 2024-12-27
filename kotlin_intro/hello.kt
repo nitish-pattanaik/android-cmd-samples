76f00f1e0e0dd71e5d5b7a64361ef151b95097d1
Nitish Pattanaik <nitishkpattanaik@gmail.com>
2024-12-27 20:29:41 +0530

    clean up

--- kotlin_intro/hello.kt
+++ kotlin_intro/hello.kt
@@ -1,5 +1,10 @@
-fun main(args : Array<String>) {
-	for (arg in args) println("arguments: $arg")
-	print("Hello world")
-	return
+// fun main(args : Array<String>) {
+// 	for (arg in args) println("arguments: $arg")
+// 	print("Hello world")
+// 	return
+// }
+
+fun main() {
+	println("Hello World")
+	return;
 }
